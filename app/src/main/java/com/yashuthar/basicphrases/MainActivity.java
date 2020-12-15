73b6bc64351ca6aff5e344adebdadc2a23bac4f5
yash17-12-98 <sutharyash40@gmail.com>
2020-12-15 21:56:46 +0530

    get translation of phrases

---
+++ app/src/main/java/com/yashuthar/basicphrases/MainActivity.java
@@ -0,0 +1,28 @@
+package com.yashuthar.basicphrases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void buttonTapped(View view){
+        int id = view.getId();
+        String ourId = "";
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.yashuthar.basicphrases");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+        mplayer.start();
+        Log.i("button tapped", ourId);
+    }
+}
